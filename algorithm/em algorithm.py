89c7a52e0301bbb81089c3a4a678c6a325cad8c6
hangy <hangyang96@163.com>
2018-10-11 10:23:07 +0800

    EM算法

---
+++ algorithm/em algorithm.py
@@ -0,0 +1,67 @@
+import numpy as np
+from scipy import stats
+
+observations = np.array([[1, 0, 0, 0, 1, 1, 0, 1, 0, 1],
+                         [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+                         [1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+                         [1, 0, 1, 0, 0, 0, 1, 1, 0, 0],
+                         [0, 1, 1, 1, 0, 1, 1, 1, 0, 1]])
+
+
+def em_single(priors, observations):
+    """
+
+    :param priors: [theta_A,theta_B]
+    :param observations: [m X n matrix]
+    :return:
+    new_priors:[new_theta_A,new_theta_B]
+    """
+    counts = {'A': {'H': 0, 'T': 0}, 'B': {'H': 0, 'T': 0}}
+    theta_A = priors[0]
+    theta_B = priors[1]
+    # E step
+    for observation in observations:
+        len_observation = len(observation)
+        # 当前硬币正面的次数
+        num_heads = observation.sum()
+        # 当前硬币反面的次数
+        num_tails = len_observation - num_heads
+        # binom.pmf 是二项分布  binom.pmf(k) = choose(n, k) * p**k * (1-p)**(n-k) p = theta_A
+        contribution_A = stats.binom.pmf(num_heads, len_observation, theta_A)
+        contribution_B = stats.binom.pmf(num_heads, len_observation, theta_B)
+        weight_A = contribution_A / (contribution_A + contribution_B)
+        weight_B = contribution_B / (contribution_A + contribution_B)
+        # 统计在当前参数下A、B硬币的正反面次数
+        counts['A']['H'] += weight_A * num_heads
+        counts['A']['T'] += weight_A * num_tails
+        counts['B']['H'] += weight_B * num_heads
+        counts['B']['T'] += weight_B * num_tails
+    # M step
+    new_theta_A = counts['A']['H'] / (counts['A']['H'] + counts['A']['T'])
+    new_theta_B = counts['B']['H'] / (counts['B']['H'] + counts['B']['T'])
+    return [new_theta_A, new_theta_B]
+
+
+def em(observations, prior, tol=1e-6, iterations=100):
+    """
+
+    :param observations: 观测数据
+    :param prior: 模型初值
+    :param tol: 迭代结束阈值
+    :param iterations: 最大迭代次数
+    :return: 局部最优模型
+    """
+    iteration = 0
+    while iteration < iterations:
+        new_prior = em_single(prior, observations)
+        delta_change = np.abs(prior[0] - new_prior[0])
+        if delta_change < tol:
+            break
+        else:
+            prior = new_prior
+            iteration += 1
+    return [new_prior, iteration]
+
+
+print(em(observations, [0.6, 0.5]))
+print(em(observations, [0.5,0.6]))
